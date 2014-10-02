81cf2c523f6657ac74de9f678c02b3e4851fc24b
Nickolas Moeckel <nickolas1@gmail.com>
2014-10-02 12:02:33 -0500

    fix html validation errors

---
+++ js/skelconfig.js
@@ -0,0 +1,9 @@
+window._skel_config = {
+    prefix: "css/style",
+    resetCSS: true,
+    boxModel: "border",
+    grid: { gutters: 0 },
+    breakpoints: {
+        'normal': { range: "1-", containers: 'fluid', useOrientation: true }, 
+    }
+};
